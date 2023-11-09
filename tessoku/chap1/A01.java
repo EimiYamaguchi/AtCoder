6f2878f562c55543f0b8e3cb70937bb680ecb8cb
EimiYamaguchi <eimi.y.1995@outlook.jp>
2023-11-09 20:10:15 +0900

    A01

---
+++ tessoku/chap1/A01.java
@@ -0,0 +1,13 @@
+package tessoku;
+
+import java.util.Scanner;
+
+public class A01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		final int N = sc.nextInt();
+		System.out.println(N * N);
+	}
+
+}
